b0dede4a0fbb5f6ce8494a12684d7a0e37fcef60
Arsen <52761327+cly4auHo@users.noreply.github.com>
2020-02-18 17:32:20 +0200

    Add files via upload

--- FX/Main.java
+++ FX/Main.java
@@ -1,11 +1,10 @@
-package application;
+package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,87 +13,60 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 public class Main extends Application {
-	Stage primaryStage;
-	PasswordField passwordField = new PasswordField();
-	TextField loginField = new TextField("");
-	@Override
-	public void start(Stage primaryStage) {
-		this.primaryStage=primaryStage;
-		try {			
-			Button ok = new Button("OK");
-			ok.setId("myId");
-			Label login = new Label("Login");
-			Label password = new Label("Password");
-			// BorderPane root = new BorderPane();
-			GridPane root = new GridPane();
-			root.setAlignment(Pos.CENTER);
-			root.setHgap(10);
-			root.setVgap(10);
-			root.setPadding(new Insets(25, 25, 25, 25));
-			root.add(login, 0, 0);
-			root.add(loginField, 1, 0);
-			root.add(password, 0, 1);
-			root.add(passwordField, 1, 1);
-			HBox hBox = new HBox();
-			hBox.getChildren().add(ok);
-			hBox.setAlignment(Pos.BASELINE_RIGHT);
-			root.add(hBox, 1, 2);
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().
-		getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-//			ok.setOnAction((event) -> {
-//				if (loginField.getText().equals("admin") && passwordField.getText().equals("123")) {
-//					System.out.println("Access Granted");
-//				} else {
-//					System.out.println("Access Denied");
-//				}
-//			});
-//		} catch (Exception e) {
-//
-//		}
-			ok.setOnAction((event) -> {
-				//nextScene();
-				nextSceneFullScreen();
-			});
-		} catch (Exception e) {
 
-		}
-	}
+    Stage setPrimacy;
 
-	private void nextScene() {
-		Label result; 
-		if (loginField.getText().equals("admin") && passwordField.getText().equals("123")) {
-			result=new Label("Access Granted");
-			result.setId("Green");
-		} else {
-			result=new Label("Access Denied");
-			result.setId("Red");
-		}
-		BorderPane root = new BorderPane();
-		root.setCenter(result);
-		Scene scene = new Scene(root, 400, 400);
-		scene.getStylesheets().add(getClass().
-	getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	private void nextSceneFullScreen() {
-		
-		BorderPane root = new BorderPane();
-		
-		Scene scene = new Scene(root, 400, 400);
-		scene.getStylesheets().add(getClass().
-	getResource("application_f.css").toExternalForm());
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		primaryStage.setFullScreen(true);
-	}
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button buttonAccept = new Button("OK");
+        Label login = new Label("Log");
+        Label password = new Label("Pas");
+        TextField loginField = new TextField();
+        PasswordField passwordField = new PasswordField();
+
+        //BorderPane bp = new BorderPane();
+        //bp.setCenter(buttonAccept);
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane gp = new GridPane();
+        Scene scene = new Scene(gp, 300, 275);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        gp.setAlignment(Pos.CENTER);
+        gp.setHgap(10);
+        gp.setVgap(10);
+        gp.setPadding(new Insets(25, 25, 25, 25));
+
+        //gp.add(buttonAccept, 2, 2);
+        gp.add(login, 1, 0);
+        gp.add(password, 1, 1);
+        gp.add(loginField, 2, 0);
+        gp.add(passwordField, 2, 1);
+
+        HBox hBox = new HBox();
+        hBox.getChildren().add(buttonAccept);
+        hBox.setAlignment(Pos.BASELINE_RIGHT);
+        gp.add(hBox, 2, 2);
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        buttonAccept.setOnAction((event) -> {
+            if (loginField.getText().equals("Admin") && passwordField.getText().equals("123")) {
+                System.out.println("Access");
+            } else {
+                System.out.println("Access denied");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
